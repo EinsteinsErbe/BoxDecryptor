770562fd5a79dd34c987ed1d534e3bd15534d880
Manuel J <Major94@users.noreply.github.com>
2020-11-08 16:05:18 +0100

    simple decryption webservice

---
+++ BoxDecryptor/BoxDecryptor/BCFileDecryptor.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing.Internal.PathSegments;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using static System.Buffer;
+
+namespace BCFileDecryptorCore
+{
+    public class BCFileDecryptor
+    {
+        private byte[] decryptedPrivateKey;
+        public bool ready = false;
+        public string currentDir;
+
+        public BCFileDecryptor(string keyFilePath, string password)
+        {
+            // ============================================
+            // AES decryption of private key in .bckey file
+            // =============================================
+
+            try
+            {
+                // collect information about the user account
+                AccountData accountInfo = new AccountData();
+                accountInfo.ParseBCKeyFile(keyFilePath);
+                accountInfo.Password = password;
+
+                // decrypt the private key from the .bckey file
+                decryptedPrivateKey = AESHelper.DecryptDataPBKDF2(
+                    accountInfo.EncryptedPrivateKey, accountInfo.Password,
+                    accountInfo.PBKDF2Salt, accountInfo.PBKDF2Iterations
+                );
+                ready = true;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void DecryptDir(string inputDir, string outputDir)
+        {
+            currentDir = outputDir;
+            Directory.CreateDirectory(outputDir);
+
+            foreach (string inputFile in Directory.EnumerateFiles(inputDir, "*.bc"))
+            {
+                string outputFile = Path.Combine(outputDir, Path.GetFileNameWithoutExtension(inputFile));
+                DecryptFile(inputFile, outputFile);
+            }
+
+            foreach (string dir in Directory.EnumerateDirectories(inputDir))
+            {
+                string outDir = Path.Combine(outputDir, Path.GetFileName(dir));
+                DecryptDir(dir, outDir);
+            }
+        }
+
+        public void DecryptFile(string inputFilePath, string outputFilePath)
+        {
+            try
+            {
+                //Console.WriteLine("Decryption process started");
+
+                // =============================================
+                // RSA decryption of file information (header)
+                // =============================================
+
+                // collect information about the file to be decrypted
+                FileData fileData = new FileData();
+
+                fileData.ParseHeader(inputFilePath, outputFilePath);
+
+                // decrypt the file key (from the header) used for decryption of file data
+                byte[] decryptedFileKey = RSAHelper.DecryptData(fileData.EncryptedFileKey, decryptedPrivateKey);
+
+                byte[] fileCryptoKey = new byte[32];
+                BlockCopy(decryptedFileKey, 32, fileCryptoKey, 0, 32);
+                // =============================================
+                // AES decryption of encrypted file
+                // =============================================
+
+                // decrypt the file data ...
+                byte[] decryptedFileBytes = AESHelper.DecryptFile(
+                    fileData.EncryptedFilePath, fileCryptoKey, fileData.BaseIVec,
+                    fileData.BlockSize, fileData.HeaderLen, fileData.CipherPadding);
+
+                File.WriteAllBytes(Path.GetFullPath(fileData.OutputFilePath), decryptedFileBytes);
+
+                //Console.WriteLine($"Successfully decrypted file '{fileData.EncryptedFilePath}', "
+                //+ $"output: '{fileData.OutputFilePath}'");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                if (e.StackTrace != null)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+            }
+        }
+    }
+}
--- BoxDecryptor/Controllers/DecryptController.cs
+++ BoxDecryptor/Controllers/DecryptController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using BCFileDecryptorCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +14,63 @@ namespace BoxDecryptor.Controllers
     [Route("[controller]")]
     public class DecryptController : ControllerBase
     {
-        [HttpGet]
-        public string Get()
+        static BCFileDecryptor decryptor;
+        public static Stopwatch sw;
+        public static bool busy;
+
+        public DecryptController() : base()
         {
-            return "Hello World";
+            if (decryptor == null)
+            {
+                string pw = Environment.GetEnvironmentVariable("PW");
+                string keyFilePath = Environment.GetEnvironmentVariable("KEY_PATH");
+                decryptor = new BCFileDecryptor(keyFilePath, pw);
+            }
+
+            if (sw == null)
+            {
+                sw = new Stopwatch();
+            }
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("info")]
+        public string GetInfo()
+        {
+            return busy ? sw.Elapsed.TotalSeconds.ToString("N2") + "s [" + decryptor.currentDir + "]" : "idle" + Environment.NewLine;
+        }
+
+        [HttpGet]
+        [Route("process")]
+        public string Decrypt()
+        {
+            if (!decryptor.ready)
+            {
+                return "Decryptor not ready" + Environment.NewLine;
+            }
+            if (busy)
+            {
+                return GetInfo();
+            }
+            busy = true;
+
+            //Start Decryption    
+            string inputDir = Environment.GetEnvironmentVariable("INPUT_DIR");
+            string outputDir = Environment.GetEnvironmentVariable("OUTPUT_DIR");
+
+            Task.Run(() =>
+            {
+                sw.Reset();
+                sw.Start();
+                decryptor.currentDir = "deleting";
+                Directory.Delete(outputDir, true);
+                decryptor.DecryptDir(inputDir, outputDir);
+                busy = false;
+                sw.Stop();
+            });
+
+            return "Decryption started" + Environment.NewLine;
         }
     }
 }
